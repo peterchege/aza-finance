3edb72b6d2817464ae359e387a56709df7ebb9f6
peterchege <peterchege442@gmail.com>
2022-06-22 18:39:06 +0300

    Update function on live test

--- controller/transaction.controller.ts
+++ controller/transaction.controller.ts
@@ -6,11 +6,12 @@ import { validateSchema } from "../utils/validator";
 
 
 export const createTransaction = async (req: Request, res: Response) => {
+
     try {
         const schema: Schema = Joi.object({
             emailAddress: Joi.string().required().email().label("Email address"),
-            inputAmount: Joi.number().required().label("Input amount"),
-            inputAmountCurrency: Joi.string().required().label("Input amount currency"),
+            inputAmount: Joi.number().greater(-1).required().label("Input amount"),
+            inputAmountCurrency: Joi.string().valid('USD', 'EUR').required().label("Input amount currency"),
             outputAmount: Joi.number().required().label("Output amount"),
             outputAmountCurrency: Joi.string().required().label("Output amount currency"),
         });
@@ -49,6 +50,28 @@ export const getTransactions = async (req: Request, res: Response) => {
     try {
         const transactions = await Transaction.findAll();
         return res.send({ success: true, message: "Transactions found", data: transactions })
+    } catch (error) {
+        console.log(error)
+        return res.send({ success: false, message: "Error Occurred" })
+    }
+}
+
+export const updateTransaction = async (req: Request, res: Response) => {
+    try {
+        const schema: Schema = Joi.object({
+            emailAddress: Joi.string().email().label("Email address"),
+            inputAmount: Joi.number().greater(-1).label("Input amount"),
+            inputAmountCurrency: Joi.string().valid('USD', 'EUR').label("Input amount currency"),
+            outputAmount: Joi.number().label("Output amount"),
+            outputAmountCurrency: Joi.string().label("Output amount currency"),
+        });
+
+        const result = validateSchema(req.body, schema);
+        if (result) return res.status(200).send({ success: false, message: result });
+        const transaction = await Transaction.update(req.body, { where: { id: req.params.transactionId } })
+        const updateInfo = await Transaction.findOne({ where: { id: req.params.transactionId } })
+        console.log(transaction);
+        return res.send({ success: true, message: "Transaction Updated successfully ", data: updateInfo })
     } catch (error) {
         console.log(error)
         return res.send({ success: false, message: "Error Occurred" })
--- routes/transaction.routes.ts
+++ routes/transaction.routes.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from "express";
-import { createTransaction, getTransactions, getTransaction } from "../controller/transaction.controller";
+import { createTransaction, getTransactions, getTransaction, updateTransaction } from "../controller/transaction.controller";
 const transactionRoute = Router();
 
 transactionRoute.post("/", createTransaction)
 transactionRoute.get("/", getTransactions)
 transactionRoute.get("/:transactionId", getTransaction)
+transactionRoute.put("/:transactionId", updateTransaction)
+
 
 export default transactionRoute
